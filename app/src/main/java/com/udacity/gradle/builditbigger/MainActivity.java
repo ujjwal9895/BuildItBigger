726cefee2c764ed6505dcd225ee26ac24d80ad81
Ujjwal Jain <ujjwal9895@gmail.com>
2016-03-14 18:18:15 +0530

    Add joke telling java library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.udacity.gradle.jokes.JokeTelling;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -40,7 +42,10 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+
+        JokeTelling jokeTelling = new JokeTelling();
+        String joke = jokeTelling.getJoke();
+        Toast.makeText(this, joke, Toast.LENGTH_LONG).show();
     }
 
 
---
+++ tellJoke/src/main/java/com/udacity/gradle/jokes/JokeTelling.java
@@ -0,0 +1,9 @@
+package com.udacity.gradle.jokes;
+
+public class JokeTelling {
+
+	public String getJoke() {
+		return "Q. Why do Java developers wear glasses? A. Bcoz they don't C#";
+	}
+	
+}
